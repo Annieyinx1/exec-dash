8ef2426afa85f8f15434f6b5dd661de3e176f5be
Annieyinx1 <65737038+Annieyinx1@users.noreply.github.com>
2020-06-13 15:55:14 -0400

    Update dashboard_generator.py

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -3,9 +3,24 @@
 """
 
 # dashboard_generator.py
+import os
+import sys
+import csv
+import plotly
+
+# define function to convert USD
+def tousd(total_sale):
+    return f"${total_sale:,.2f}"
 
 print("-----------------------")
-print("MONTH: March 2018")
+# prompt user input for month selection
+# reference a file in the "data" directory
+# ... adapted from: https://github.com/Annieyinx1/intro-to-python/data/monthly-sales
+#csv_filepath = os.path.join(os.path.dirname(__file__), ".","intro-to-python", csv_filename)
+
+
+
+print("MONTH")
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
